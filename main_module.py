cdefd4d34b9b6c64889cbb4e806ee34d591846d5
bacchus <bacchustut@gmail.com>
2019-01-11 21:58:20 +0300

    a little update

--- main_module.py
+++ main_module.py
@@ -14,21 +14,9 @@ import math
 import inspect
 
 
-def whoami():
-    """
-    Returns current function name
-    """
-    return inspect.stack()[1][3]
-def have_any_dimensions(columnHeader):
-	if 'dimensions' in columnHeader.keys():
-		return True
-	else:
-		return False
-
 SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
 KEY_FILE_LOCATION = None
 VIEW_ID = None
-
 
 body={
 'reportRequests': [
@@ -42,6 +30,14 @@ body={
 
 class ReportSampled(Exception): pass
 class CredentialsError(Exception): pass
+class VIEWIDError(Exception): pass
+
+def whoami():
+    """
+    Made for exceptions raising
+    Returns current function name
+    """
+    return inspect.stack()[1][3]
 
 def get_credentials(KEY_FILE_LOCATION):
     """Initializes an Analytics Reporting API V4 service object.
@@ -49,45 +45,53 @@ def get_credentials(KEY_FILE_LOCATION):
       An authorized Analytics Reporting API V4 service object.
     """
     if KEY_FILE_LOCATION is None:
-    	raise CredentialsError('credentials error in {0}'.format(whoami()))
+        raise CredentialsError('credentials error in {0}'.format(whoami()))
+    # get token
     credentials = ServiceAccountCredentials.from_json_keyfile_name(KEY_FILE_LOCATION, SCOPES)
 
-    # Build the service object.
+    # build the service object.
     analytics = build('analyticsreporting', 'v4', credentials=credentials)
     return analytics
 
-def report_to_dataframe(report):    
+def report_prettify(report, is_df = True):
     """
-    Converts peace of analytics report in data frame.
-    """
-    columns=[] 
-    columnHeader=report.get('columnHeader', {})
-    if have_any_dimensions(columnHeader):
-	    for dimension in columnHeader['dimensions']:
-	        columns.append(dimension)
-	    for metric in columnHeader['metricHeader']['metricHeaderEntries']:
-	        columns.append(metric.get('name'))
+    Makes report more convenient
+    if is_df:
+        returns df
     else:
-    	for metric in columnHeader['metricHeader']['metricHeaderEntries']:
-	        columns.append(metric.get('name'))
+        returns dict
+    """
+    #get header of columns and create objects to fill
+    columnHeader = report.get('columnHeader', {})
+    data = {}
+    dimensions = []
+    metrics = []
 
-    rows_to_add={}
-    count=0
-    for row in report['data']['rows']:
-        row_to_add={}
-        if have_any_dimensions(columnHeader):
-        	len_dimensions = len(columnHeader['dimensions'])
-        else:
-        	len_dimensions = 0
-        len_metrics=len(columnHeader['metricHeader']['metricHeaderEntries'])
-        for i in range(len_dimensions):
-           row_to_add[columns[i]]=row['dimensions'][i]
-        for i in range(len_metrics):
-            row_to_add[columns[i+len_dimensions]]=row['metrics'][0]['values'][i]
-        rows_to_add[count]=row_to_add
-        count+=1
-    df=pd.DataFrame().from_dict(rows_to_add, orient='index')
-    return df
+    #creating data dict and lists of columns names
+    for dimension in columnHeader.get('dimensions',[]):
+        name = dimension.split(':')[1]
+        dimensions.append(name)
+        data[name] = []
+    for metric in columnHeader['metricHeader']['metricHeaderEntries']:
+        name = metric['name'].split(':')[1]
+        metrics.append(name)
+        data[name] = []
+
+    # dimensions may not be, they are not required in request
+    if dimensions:
+        for row in report['data']['rows']:
+            for i,value in enumerate(row['dimensions']):
+                data[dimensions[i]].append(value)
+            for i,value in enumerate(row['metrics'][0]['values']):
+                data[metrics[i]].append(value)
+    else:
+        for row in report['data']['rows']:
+            for i,value in enumerate(row['metrics'][0]['values']):
+                data[metrics[i]].append(value)
+    if is_df:
+        return pd.DataFrame().from_dict(data)
+    else:
+        return data
 
 def is_report_sapled(report):
     """
@@ -100,30 +104,30 @@ def is_report_sapled(report):
             flag=True
     return flag
 
-def report_to_list_dfS(body,VIEW_ID,anal_cred,size=10000,list_df=None):
+def responce_to_list_reports(body,VIEW_ID,anal_cred,size=10000,list_reports=None):
     """
-    Returns a list of data frames from a whole analytics report
-    or appends to list you set
+    Returns a list of data objects from responce
+    or appends to list you have set
     """
-    if  not list_df:
-        list_df=[]
+    if  not list_reports:
+        list_reports=[]
     if VIEW_ID is None:
     	raise VIEWIDError('in functioon {0}'.format(whoami()))
     page_token=0
-    body['reportRequests'][0]['pageSize']=size
+    body['reportRequests'][0]['pageSize']=pageSize
     body['reportRequests'][0]['pageToken']=str(page_token)
     
     responce=anal_cred.reports().batchGet(body=body).execute()
     report=responce['reports'][0]
     if not is_report_sapled(report):
         num_pages = math.ceil(report['data']['rowCount'] / size)
-        list_df.append(report_to_dataframe(report))
+        list_reports.append(report_prettify(report))
 
         for i in range(1,num_pages):
             body['reportRequests'][0]['pageToken']=str(i*size)
             responce=anal_cred.reports().batchGet(body=body).execute()
             report=responce['reports'][0]
-            list_df.append(report_to_dataframe(report))
-        return list_df
+            list_reports.append(report_prettify(report))
+        return list_reports
     else:
         raise ReportSampled('Report is sampled')
